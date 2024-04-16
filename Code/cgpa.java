7f26fe85e2542382cf6d12f4ff6febd5675576ee
Kishore <128295553+Kishore-june@users.noreply.github.com>
2024-04-16 23:23:07 +0530

    Add files via upload

---
+++ Code/cgpa.java
@@ -0,0 +1,55 @@
+package LANGUAGE;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class cgpa implements cgpa {
+    public static void main(String[] args) {
+        System.out.println("Fill the Answer to get CGPA Accuarte result");
+        System.out.print("How Many Subject Do You Have ");
+        Scanner get = new Scanner(System.in);
+        final int totalSubject = get.nextInt();
+        // int Subject = get.nextInt();
+        // System.out.println("So,You Have " + Subject);
+        // System.out.println("enter the marks");
+        // int getValue = Subject + 1;
+        // for (int i = 1; i <= Subject; i++) {
+        // int numbers[] = new int[Subject];
+        // System.out.print(i + " ");
+        // if (i != getValue)
+        // numbers[i] = get.nextInt();
+        // else
+        // System.out.println("that's it");
+        String subMark[][] = new String[totalSubject][2];
+        System.out.println("enter all the subject that you have \"Press enter for each subject\"");
+        for (int i = 0; i < (totalSubject); i++)
+            subMark[i][0] = get.next();
+        System.out.println("Enter the Marks rescpective for the subjeact");
+        for (int j = 0; j < totalSubject; j++) {
+            System.out.print(subMark[j][0] + " = ");
+            subMark[j][1] = get.next();
+        }
+        int total = 0;
+        for (int i = 0; i < (totalSubject); i++)
+            total = total + Integer.parseInt(subMark[i][1]);
+        float verification = (total) / (totalSubject);
+
+        System.out.print("For How Many mark do you write for your exam  ");
+        byte examMark = get.nextByte();
+        float result = 0;
+
+        if (verification < examMark) {
+            int divider = (examMark * totalSubject) / 10;
+            Float dividerResult = (float) (examMark * totalSubject) / divider;
+            result = verification / dividerResult;
+
+        } else
+            System.out.println("error check the subject marks and exam mark properly");
+
+        System.out.println(result);
+
+    }
+}
