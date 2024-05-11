8ec20b6e35898a664654ddabeae4fb5db9803d3f
Kishore <128295553+Kishore-june@users.noreply.github.com>
2024-05-11 11:57:54 +0530

    Update cgpa.java

--- Code/cgpa.java
+++ Code/cgpa.java
@@ -1,44 +1,40 @@
-package LANGUAGE;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner6;
-
-public class cgpa implements cgpa {
+public class CGPACalculator {
     public static void main(String[] args) {
-        System.out.println("Fill the Answer to get CGPA Accuarte result");
-        System.out.print("How Many Subject Do You Have ");
-        Scanner get = new Scanner(System.in);
-        final int totalSubject = get.nextInt();
+        System.out.println("Fill the Answer to get CGPA Accurate result");
+        System.out.print("How Many Subject Do You Have: ");
+        Scanner scanner = new Scanner(System.in);
+        final int totalSubject = scanner.nextInt();
         String subMark[][] = new String[totalSubject][2];
-        System.out.println("enter all the subject that you have \"Press enter for each subject\"");
-        for (int i = 0; i < (totalSubject); i++)
-            subMark[i][0] = get.next();
-        System.out.println("Enter the Marks rescpective for the subjeact");
+        System.out.println("Enter all the subject that you have (Press enter after each subject)");
+        for (int i = 0; i < totalSubject; i++)
+            subMark[i][0] = scanner.next();
+        System.out.println("Enter the Marks respective for the subjects");
         for (int j = 0; j < totalSubject; j++) {
             System.out.print(subMark[j][0] + " = ");
-            subMark[j][1] = get.next();
+            subMark[j][1] = scanner.next();
         }
         int total = 0;
-        for (int i = 0; i < (totalSubject); i++)
-            total = total + Integer.parseInt(subMark[i][1]);
-        float verification = (total) / (totalSubject);
+        for (int i = 0; i < totalSubject; i++)
+            total += Integer.parseInt(subMark[i][1]);
+        float averageMark = (float) total / totalSubject;
 
-        System.out.print("For How Many mark do you write for your exam  ");
-        byte examMark = get.nextByte();
-        float result = 0;
+        System.out.print("Enter the total mark for your exam: ");
+        int examMark = scanner.nextInt();
+        float cGPA = 0;
 
-        if (verification < examMark) {
+        if (averageMark < examMark) {
             int divider = (examMark * totalSubject) / 10;
-            Float dividerResult = (float) (examMark * totalSubject) / divider;
-            result = verification / dividerResult;
+            float dividerResult = (float) (examMark * totalSubject) / divider;
+            cGPA = averageMark / dividerResult;
 
-        } else
-            System.out.println("error check the subject marks and exam mark properly");
+        } else {
+            System.out.println("Error: Please check the subject marks and exam mark properly");
+        }
 
-        System.out.println(result);
+        System.out.println("Your CGPA: " + cGPA);
 
     }
 }
